2f1fca40b3bb1707b989aa987fdf1d1a0d441c74
Krishna Patil <128321437+krishnapatil6446@users.noreply.github.com>
2025-11-16 15:21:12 +0530

    first commit

---
+++ app/about/company/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <main>
+      <h1>Company</h1>
+      <p>Content coming soon…</p>
+    </main>
+  );
+}
---
+++ app/products/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <main>
+      <h1>Products</h1>
+      <p>Content coming soon…</p>
+    </main>
+  );
+}
